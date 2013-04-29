a6a833f87f61c98df8e0d66e62b5bf9dcc6430c6
arun.singh10@wipro.com <arun.singh10@wipro.com>
2013-04-29 16:13:31 +0530

    due to some changes

--- src/main/java/com/wipro/atlassian/App.java
+++ src/main/java/com/wipro/atlassian/App.java
@@ -3,6 +3,12 @@ package com.wipro.atlassian;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Appmain()");
+		System.out.println("Appmain()");
+		System.out.println("Appmain()");
+		System.out.println("Appmain);
+		System.out.println("Apain()");
+		System.out.println("ppmain()");
+		System.out.println("Appmain()");
 	}
 	public String simplemethod() {
 		return "wipro";
